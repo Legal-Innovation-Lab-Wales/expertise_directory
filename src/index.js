50d25c1b7b93668a79cf37d1731944dc74781713
Ieuan Skinner <ieuanskinner@gmail.com>
2021-02-18 15:17:34 +0000

    Hotfix - The correct lilw-react-components package is now being used

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import Directory from './components/directory'
-import {Header,Footer} from 'lilw-react-components-test'
+import {Header,Footer} from 'lilw-react-components'
 
 ReactDOM.render([
   <Header a11y_header_desc='Expertise Directory'/>,
